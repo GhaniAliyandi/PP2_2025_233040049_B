d53a4f9ccb8964fe8d353b8a1436e98a8e485711
GhaniAliyandi <ghanialiyandi12345@gmail.com>
2025-11-03 19:42:37 +0700

    Tugas

---
+++ src/main/java/id/ac/unpas/mavenproject4/Mavenproject4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package id.ac.unpas.mavenproject4;
+
+/**
+ *
+ * @author ghani aliyandi
+ */
+public class Mavenproject4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/id/ac/unpas/mavenproject4/Modul5/Latihan3.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.unpas.mavenproject4.Modul5;
+
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author ghani aliyandi
+ */
+public class Latihan3 {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                JFrame frame = new JFrame("Label dan Tombol");
+                frame.setSize(400, 300);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                
+                // 1. Atur Layout Manager
+                // FlowLayout akan menyusun komponen dari kiri ke kanan.
+                frame.setLayout(new FlowLayout());
+                
+                // 2. Buat komponen GUI
+                JLabel label = new JLabel("Teks Awal");
+                JButton button = new JButton("Klik Saya!");
+                
+                /* 3. Tambahkan Aksi (ActionListener) ke tombol
+                   penambahan ini menggunakan lambda untuk mempersingkat
+                   penggunaan anonymous inner class */
+                button.addActionListener(e -> {
+                    // Aksi ini akan mengubah teks pada label
+                    label.setText("Tombol berhasil diklik!");
+                });
+                
+                /* 4. Tambahkan komponen ke frame
+                   karena kita menggunakan FlowLayout,
+                   keduanya akan tampil berdampingan */
+                frame.add(label);
+                frame.add(button);
+                
+                frame.setVisible(true);
+            }
+        });
+    
+    }
+}
