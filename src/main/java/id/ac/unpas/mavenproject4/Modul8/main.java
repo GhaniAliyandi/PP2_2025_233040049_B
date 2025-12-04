b70b6021133aa5ba409ab00f0158a2798abc3d77
GhaniAliyandi <ghanialiyandi12345@gmail.com>
2025-12-04 14:14:22 +0700

    Modul8

---
+++ src/main/java/id/ac/unpas/mavenproject4/Modul8/main.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.unpas.mavenproject4.Modul8;
+
+import id.ac.unpas.mavenproject4.Modul8.controller.PersegiPanjangController;
+import id.ac.unpas.mavenproject4.Modul8.model.PersegiPanjangModel;
+import id.ac.unpas.mavenproject4.Modul8.view.PersegiPanjangView;
+
+/**
+ *
+ * @author ghani aliyandi
+ */
+public class main {
+    public static void main(String[] args) {
+        // 1. Instansiasi Model
+        PersegiPanjangModel model = new PersegiPanjangModel();
+
+        // 2. Instansiasi View
+        PersegiPanjangView view = new PersegiPanjangView();
+
+        // 3. Instansiasi Controller (Hubungkan Model & View)
+        PersegiPanjangController controller = new PersegiPanjangController(model, view);
+
+        // 4. Tampilkan View
+        view.setVisible(true);
+    }
+}
